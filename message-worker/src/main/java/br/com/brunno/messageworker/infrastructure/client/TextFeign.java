2417e04ff391214e66a6107fb26e6f3a07540cc1
unknown <brunnosantana98@gmail.com>
2023-11-28 21:08:13 -0300

    adicionando docker-compose

--- message-worker/src/main/java/br/com/brunno/messageworker/infrastructure/client/TextFeign.java
+++ message-worker/src/main/java/br/com/brunno/messageworker/infrastructure/client/TextFeign.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "localhost:8082", name = "TextFeign")
+@FeignClient(url = "${feign.client.config.text.url}", name = "TextFeign")
 public interface TextFeign {
 
     @GetMapping("/message/{message}")
